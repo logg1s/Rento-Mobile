319d05dd98660a0906e0389c883e1b486b138f72
logg1s <lrng159@gmail.com>
2025-02-05 07:48:35 +0700

    remove unnecessary component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 
 const TabHome = () => {
   return (
     <View className="bg-secondary-100">
-      <Text className="font-bold text-secondary-100">TabHome</Text>
+      <TouchableOpacity className="bg-secondary rounded-full w-[150px] px-1 py-5 justify-center items-center">
+        <Text className="text-base text-white font-pmedium">Continue</Text>
+      </TouchableOpacity>
     </View>
   );
 };
