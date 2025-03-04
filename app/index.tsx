d25fdb78cc7d3c5e94be6a83efdbcb4d7dbf9a57
l0gg1s <lrng159@gmail.com>
2025-03-04 23:30:55 +0700

    research notification

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,182 @@
-import { View, ImageBackground } from "react-native";
-import React, { useEffect, useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import CustomButton from "@/components/CustomButton";
-import { Redirect, router } from "expo-router";
-import useAuthStore from "@/stores/authStore";
+// import { View, ImageBackground } from "react-native";
+// import React, { useEffect, useState } from "react";
+// import { SafeAreaView } from "react-native-safe-area-context";
+// import CustomButton from "@/components/CustomButton";
+// import { Redirect, router } from "expo-router";
+// import useAuthStore from "@/stores/authStore";
+//
+// const WelcomeScreen = () => {
+//   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+//   return (
+//     <>
+//       {isLoggedIn ? (
+//         <Redirect href="/home" />
+//       ) : (
+//         <SafeAreaView className="flex-1">
+//           <ImageBackground
+//             source={require("@/assets/images/rento-logo-blue.jpg")}
+//             resizeMode="cover"
+//             className="w-full h-full items-center justify-center"
+//           >
+//             <View className={`gap-5 w-1/2 absolute bottom-24 mb-5`}>
+//               <CustomButton
+//                 title="Đăng nhập"
+//                 textStyles="text-white"
+//                 onPress={() => router.push("/login")}
+//               />
+//               <CustomButton
+//                 title="Đăng ký"
+//                 outline
+//                 onPress={() => router.push("/signup")}
+//               />
+//             </View>
+//           </ImageBackground>
+//         </SafeAreaView>
+//       )}
+//     </>
+//   );
+// };
+//
+// export default WelcomeScreen;
+import { useState, useEffect, useRef } from "react";
+import { Text, View, Button, Platform } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
 
-const WelcomeScreen = () => {
-  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+async function sendPushNotification(expoPushToken: string) {
+  const message = {
+    to: expoPushToken,
+    sound: "default",
+    title: "Original Title",
+    body: "And here is the body!",
+    data: { someData: "goes here" },
+  };
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+function handleRegistrationError(errorMessage: string) {
+  alert(errorMessage);
+  throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      handleRegistrationError(
+        "Permission not granted to get push token for push notification!",
+      );
+      return;
+    }
+    const projectId =
+      Constants?.expoConfig?.extra?.eas?.projectId ??
+      Constants?.easConfig?.projectId;
+    if (!projectId) {
+      handleRegistrationError("Project ID not found");
+    }
+    try {
+      const pushTokenString = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(pushTokenString);
+      return pushTokenString;
+    } catch (e: unknown) {
+      handleRegistrationError(`${e}`);
+    }
+  } else {
+    handleRegistrationError("Must use physical device for push notifications");
+  }
+}
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  const notificationListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current,
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
-    <>
-      {isLoggedIn ? (
-        <Redirect href="/home" />
-      ) : (
-        <SafeAreaView className="flex-1">
-          <ImageBackground
-            source={require("@/assets/images/rento-logo-blue.jpg")}
-            resizeMode="cover"
-            className="w-full h-full items-center justify-center"
-          >
-            <View className={`gap-5 w-1/2 absolute bottom-24 mb-5`}>
-              <CustomButton
-                title="Đăng nhập"
-                textStyles="text-white"
-                onPress={() => router.push("/login")}
-              />
-              <CustomButton
-                title="Đăng ký"
-                outline
-                onPress={() => router.push("/signup")}
-              />
-            </View>
-          </ImageBackground>
-        </SafeAreaView>
-      )}
-    </>
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
+    >
+      <Text>Your Expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{" "}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{" "}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
+      </View>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification(expoPushToken);
+        }}
+      />
+    </View>
   );
-};
-
-export default WelcomeScreen;
+}
